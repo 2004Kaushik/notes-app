b177ced07ac0b4cd7c846a4d5b65dfa81b72493e
2004Kaushik <kaushikgaitonde.2004@gmail.com>
2025-06-10 00:02:57 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express, { Router } from 'express';
 import cors from 'cors';
+import path from "path";
 
 import router from './routes/notesRoutes.js';
 import { connectDB } from '../config/db.js';
@@ -7,16 +8,27 @@ import ratelimiter from '../middleware/ratelimiter.js';
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // middleware
-app.use(cors({
-    origin: "http://localhost:5173", // Your frontend origin
-    credentials: true               // If you're using cookies / sessions
-}));
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173", // Your frontend origin
+        credentials: true               // If you're using cookies / sessions
+    }));
+}
 app.use(ratelimiter);
 app.use(express.json());
 
 app.use("/api/notes", router);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
